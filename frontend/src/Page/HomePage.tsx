035e1128e30b31698827ea1d24f8964283362ddd
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-07-28 15:25:19 +0530

    home page Completed

--- frontend/src/Page/HomePage.tsx
+++ frontend/src/Page/HomePage.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import CompenyCard from '../Component/CompenyCard'
 
 const HomePage = () => {
-  const [compenies,setCompenies]=useState([1,2,3])
+  const [companies,setCompanies]=useState([1,2,3])
   return (
     <div >
-      <h1 className='text-4xl text-center py-5 font-semibold'>Current Hiring Compenies</h1>
+      <h1 className='text-4xl text-center py-5 font-semibold'>Current Hiring Companies</h1>
 
       <div className='px-36 py-5 space-y-2'>
-        {compenies.map((item) => <CompenyCard />)}
+        {companies.map((item) => <CompenyCard />)}
       </div>
 
     </div>
