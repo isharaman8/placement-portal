5285035bf76be74fbab3158cb6f476120dd03db7
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-07-28 12:35:08 +0530

    route added successfully

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import NotificationCard from './Component/NotificationCard'
+import AllUser from './Page/AllUser'
+import Compney from './Page/Compney'
+import HomePage from './Page/HomePage'
+import PlacementData from './Page/PlacementData'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="">
+     
+      <Routes>
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path='/notification' element={<NotificationCard />}></Route>
+        <Route path='/alluser' element={<AllUser />}></Route>
+        <Route path="/compeny" element={<Compney/>}></Route>
+        <Route path="placementdata" element={<PlacementData/>}></Route>
+      </Routes>
+     
+      
     </div>
   )
 }
---
+++ frontend/src/Component/NotificationCard.tsx
@@ -0,0 +1,13 @@
+
+
+const NotificationCard = () => {
+  return (
+    <div>
+      <h1>
+        this is notification cart
+      </h1>
+    </div>
+  )
+}
+
+export default NotificationCard
