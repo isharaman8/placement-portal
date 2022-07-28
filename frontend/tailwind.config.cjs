a1944a011ea49f8157edb78030f764cb361abc26
Vaibhav Darvekar <vabdarwekar00@gmail.com>
2022-07-28 14:56:37 +0530

    Branch merging successfully done

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -23,14 +22,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> ashok
