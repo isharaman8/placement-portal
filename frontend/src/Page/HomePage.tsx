9408374aca103472e0c345b4b60f25d410459410
Ashokzarmariya <ashokzarmariya47@gmail.com>
2022-07-28 13:52:46 +0530

    home page Completed

--- frontend/src/Page/HomePage.tsx
+++ frontend/src/Page/HomePage.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CompenyCard from '../Component/CompenyCard'
 
 const HomePage = () => {
+  const [compenies,setCompenies]=useState([1,2,3])
   return (
-    <div>
-      this is home page
+    <div >
+      <h1 className='text-4xl text-center py-5 font-semibold'>Current Hiring Compenies</h1>
+
+      <div className='px-36 py-5 space-y-2'>
+        {compenies.map((item) => <CompenyCard />)}
+      </div>
+
     </div>
   )
 }
