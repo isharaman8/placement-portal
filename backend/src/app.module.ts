439b292a826e6cb05130aac340e4ea745aa40d33
Aman Kumar <isharaman8@gmail.com>
2022-07-29 10:24:10 +0530

    finished backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CompaniesModule } from './companies/companies.module';
+// import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CompaniesModule, AuthModule],
+  imports: [
+    // ConfigModule.forRoot({ isGlobal: true }),
+    CompaniesModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
